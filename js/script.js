334b498c85873fd10ef46d5edc327b21f4e0e9e4
Gerard Kabre <gerard.kabre@gmail.com>
2017-11-13 15:17:38 +0100

    added Chart.js + Api values form CoinMarketCap

--- js/script.js
+++ js/script.js
@@ -1,41 +1,89 @@
 var button = document.querySelector("#refresh"); 
-
 var price = document.querySelector("#price");
-var currency = "EUR";
-
-button.addEventListener("click", priceRequest);
+var ctx = document.getElementById("myChart").getContext('2d');
 
 
-function priceRequest(){
-
-    var XHR = new XMLHttpRequest();
-    XHR.onreadystatechange = function(){
-        if (XHR.readyState == 4 && XHR.status == 200 ) {
-            var data = JSON.parse(XHR.responseText); 
-            var currentEurPrice = data.bpi[currency].rate; 
-            price.textContent = currentEurPrice + " " + currency; 
-        }
-    }
-
-    XHR.open("GET", "https://api.coindesk.com/v1/bpi/currentprice.json");
-    XHR.send(); 
-
-}
-
-priceRequest();
-
-
-fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+fetch("https://api.coinmarketcap.com/v1/ticker/")
 .then((response) => {
-    console.log(response);
     return response.json() 
   })
   .then((data) => {
-    var eurRate = data.bpi.EUR.rate;
-    console.log(eurRate);
+      
+    var bitcoin = data[0].price_usd;
+    var etherum = data[1].price_usd;
+    var bitcoinCash = data[2].price_usd;
+    
+    var filteredData = data.filter((x) => x.price_usd > 100 && x.price_usd < 2000)
+
+   
+
+    var eurRate = bitcoin; 
+    price.textContent = eurRate;
+
+
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: [
+                {
+                    label: 'Price',
+                    data: [],
+                    backgroundColor: 'rgba(90, 247, 250, 0.1)',
+                    borderColor: 'rgba(90, 247, 250, 1)',
+                        /*'rgba(90, 247, 250, 1)',
+                         *'rgba(255, 146, 224, 1)',
+                         */   
+                    borderWidth: 1
+                }
+            ]
+        },
+        options: {
+            legend: {
+                display: false,
+            },
+            scales: {
+                xAxes: [{
+                    stacked: false,
+                    beginAtZero: true,
+                    scaleLabel: {
+                        labelString: 'Month'
+                    },
+                    ticks: {
+                        stepSize: 1,
+                        min: 0,
+                        autoSkip: false
+                    }
+                }]
+            }
+        }
+    });
+
+    for(var i = 0; i < data.length; i++) {
+        if (data[i].price_usd > 0 && data[i].price_usd < 10) {
+        myChart.data.datasets[0].data.push(data[i].price_usd); 
+        myChart.data.labels.push(data[i].id);
+        /*
+        myChart.data.datasets[0].backgroundColor.push('rgba(255, 146, 224, 0)'); 
+        myChart.data.datasets[0].borderColor.push('rgba(255, 146, 224, 0)'); 
+        */
+
+        myChart.update();
+    }
+
+        
+           
+            
+
+
+     }
+    
 });
 
 
+
+//Añadir Barra para seleciconar el mínimo de precio con .filter() 
+//1 solo gráfico de barras con el precio de todas las cryptos mostradas en ese momento por el graph. 
 
 
 
