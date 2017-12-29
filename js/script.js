f4826ca15696902ced98010184882de0a622cd99
Gerard Cabrerizo <gerard.kabre@gmail.com>
2017-12-29 21:39:27 +0100

    Refactoring - Filter&Display Method divided in 2 methods, deleted CleanData method, deleting data arrays and using only 1 array of data

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ class App {
   getElements() {
     this.min = document.getElementById("min");
     this.max = document.getElementById("max");
-    this.individuals = document.querySelector(".individuals");
+    this.individualsContainer = document.querySelector(".individualsContainer");
   }
   addEvents() {
     this.min.addEventListener("input", this.minEventHandler.bind(this));
@@ -16,72 +16,45 @@ class App {
   minEventHandler(event) {
     var minValue = parseFloat(event.target.value);
     var maxValue = parseFloat(this.max.value);
-    this.filterAndDisplayData(minValue, maxValue);
+    this.filterData(minValue, maxValue);
   }
   maxEventHandler() {
     var minValue = parseFloat(min.value);
     var maxValue = parseFloat(event.target.value);
-    this.filterAndDisplayData(minValue, maxValue);
+    this.filterData(minValue, maxValue);
   }
   getData() {
     fetch("https://api.coinmarketcap.com/v1/ticker/")
       .then(response => response.json())
       .then(data => (this.data = data))
-      .then(data => this.cleanData());
+      .then(data => this.filterData(20, 20000));
   }
-  cleanData() {
-    this.data.sort((a, b) => parseFloat(a.price_usd) > parseFloat(b.price_usd) ? -1
-          : parseFloat(a.price_usd) < parseFloat(b.price_usd) ? 1 : 0);
+  filterData(min, max) {
+    this.filteredItems = this.data.filter(x => x.price_usd > min && x.price_usd < max)
+    .sort((a, b) => parseFloat(a.price_usd) > parseFloat(b.price_usd) ? -1
+    : parseFloat(a.price_usd) < parseFloat(b.price_usd) ? 1 : 0);
 
-    this.prices = this.data.map(x => parseFloat(x.price_usd).toFixed(2));
-    this.ids = this.data.map(x => x.id);
-    this.dayChange = this.data.map(x => x.percent_change_24h);
-    this.weekChange = this.data.map(x => x.percent_change_7d);
-
-    this.filterAndDisplayData(20, 20000);
-  }
-  filterAndDisplayData(min, max) {
-    //Filter the data of the array based one the price.
-    var itemsToShow = this.prices.filter(x => x > min && x < max);
-    /* we get an array with the items we want to show in the prices array
-      * so now we get the index of those items to be able to also get his 
-      * labels from the label array */
-
-    var itemsToShowIndex = itemsToShow.map(x => this.prices.indexOf(x));
-    /* We pass the values of our data to the arrays that will show the data of the chart. 
-      * "x" is the index we want to show from the arrays and update the chart */
-    myChart.data.datasets[0].data = itemsToShowIndex.map(x => this.prices[x]);
-    myChart.data.labels = itemsToShowIndex.map(x => this.ids[x]);
+    myChart.data.datasets[0].data = this.filteredItems.map(x => parseFloat(x.price_usd).toFixed(2));
+    myChart.data.labels = this.filteredItems.map(x => x.id);
     myChart.update();
-
-    // Create div's under the chart with individual crypto data
-    var data = myChart.data.datasets[0].data;
-    var labels = myChart.data.labels;
-    var dayChangeToShow = itemsToShowIndex.map(x => this.dayChange[x]);
-    var weekChangeToShow = itemsToShowIndex.map(x => this.weekChange[x]);
-    this.individuals.innerHTML = "";
-
-    for (var i = 0; i < itemsToShow.length; i++) {
-      //Create each individual div with it's inside content
+    this.displayData();
+  }
+  displayData() {
+    this.individualsContainer.innerHTML = "";
+    this.filteredItems.map(x => {
       var individualDiv = document.createElement("div");
       individualDiv.classList.add("individual");
       individualDiv.innerHTML = ` 
-          <div class="left"> ${labels[i]} </div> 
-          <div class="right"> $${data[i]} </div> 
-          <div class="right daychange"> ${dayChangeToShow[i]}% </div>         
-          <div class="right weekchange"> ${weekChangeToShow[i]}% </div>`;
-      this.individuals.appendChild(individualDiv);
-
-      // change color from % changes Positive or negative.
-      var days = document.querySelectorAll(".daychange");
-      var weeks = document.querySelectorAll(".weekchange");
-      dayChangeToShow[i] > 0
-        ? days[i].classList.add("positive")
-        : days[i].classList.add("negative");
-      weekChangeToShow[i] > 0
-        ? weeks[i].classList.add("positive")
-        : weeks[i].classList.add("negative");
-    }
+        <div class="left"> ${x.id}</div> 
+        <div class="right"> $${x.price_usd}</div> 
+        <div class="right ${x.percent_change_24h > 0 ? "positive" : "negative"}"> 
+            ${x.percent_change_24h}% 
+        </div>         
+        <div class="right ${x.percent_change_7d > 0 ? "positive" : "negative"} "> 
+            ${x.percent_change_7d}% 
+        </div>`;
+      this.individualsContainer.appendChild(individualDiv);
+    })
   }
 }
 
